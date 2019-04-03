9805b4623aec84a2ea184356fbc427a1d8d5caa8
Arne de Bruijn <mail@arnedebruijn.nl>
2019-04-03 21:09:21 +0200

    Added presetdata mod to load custom projdata.txt / robotdata.txt

---
+++ GameMod/PresetData.cs
@@ -0,0 +1,60 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace GameMod
+{
+    static class DataReader
+    {
+        public static string GetData(TextAsset ta, string filename)
+        {
+            string dir = Environment.GetEnvironmentVariable("OLMODDIR");
+            try
+            {
+                return File.ReadAllText(dir + Path.DirectorySeparatorChar + filename);
+            }
+            catch (FileNotFoundException)
+            {
+            }
+            return ta.text;
+        }
+        public static string GetProjData(TextAsset ta)
+        {
+            return GetData(ta, "projdata.txt");
+        }
+        public static string GetRobotData(TextAsset ta)
+        {
+            return GetData(ta, "robotdata.txt");
+        }
+    }
+
+    [HarmonyPatch(typeof(Overload.ProjectileManager), "ReadProjPresetData")]
+    class ReadProjPresetDataPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var code in instructions)
+                if (code.opcode == OpCodes.Callvirt && ((MethodInfo)code.operand).Name == "get_text")
+                    yield return new CodeInstruction(OpCodes.Call, typeof(DataReader).GetMethod("GetProjData"));
+                else
+                    yield return code;
+        }
+    }
+
+    [HarmonyPatch(typeof(RobotManager), "ReadPresetData")]
+    class ReadRobotPresetDataPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var code in instructions)
+                if (code.opcode == OpCodes.Callvirt && ((MethodInfo)code.operand).Name == "get_text")
+                    yield return new CodeInstruction(OpCodes.Call, typeof(DataReader).GetMethod("GetRobotData"));
+                else
+                    yield return code;
+        }
+    }
+}
