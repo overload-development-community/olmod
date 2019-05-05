2436c0418838d79f7e22e632203c8844b6677556
Arne de Bruijn <mail@arnedebruijn.nl>
2019-05-05 18:52:22 +0200

    Allow pasting MP password with Ctrl+V

---
+++ GameMod/MPPwdPaste.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using Overload;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace GameMod
+{
+    [HarmonyPatch(typeof(MenuManager), "MpMatchSetup")]
+    class MPPwdPaste
+    {
+        static void Prefix()
+        {
+            if ((MenuManager.m_menu_micro_state == 1 || MenuManager.m_menu_micro_state == 4) && UIManager.m_menu_selection == 11 &&
+                ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyDown(KeyCode.V)))
+                MenuManager.mms_match_password = GUIUtility.systemCopyBuffer;
+        }
+    }
+}
