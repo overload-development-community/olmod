b88f6522e2e148ce3764b3b6d99bd0882a348e4b
Arne de Bruijn <mail@arnedebruijn.nl>
2019-05-05 18:52:03 +0200

    Allow shoot to open doors in MP

---
+++ GameMod/MPDoors.cs
@@ -0,0 +1,17 @@
+﻿using Harmony;
+using Overload;
+using UnityEngine;
+
+namespace GameMod
+{
+    [HarmonyPatch(typeof(RUtility), "CanObjectOpenDoor")]
+    internal class MPShootDoors
+    {
+        // In Multiplayer, players now open doors by shooting them or touching them.
+        private static void Postfix(GameObject go, ref bool __result)
+        {
+            if (go != null && (go.layer == 13 || go.layer == 9 || go.layer == 31) && GameplayManager.IsMultiplayerActive)
+                __result = true;
+        }
+    }
+}
