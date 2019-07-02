9faac0d9d72d56e5b4c59f4e8a8361cd5d52844c
Arne de Bruijn <mail@arnedebruijn.nl>
2019-07-02 23:22:20 +0200

    Add settings file reading

--- GameMod/Config.cs
+++ GameMod/Config.cs
@@ -1,17 +1,44 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace GameMod
 {
     static class Config
     {
+        public static string OLModDir;
         public static bool NoDownload;
+        public static JObject Settings;
+
+        private static void LoadSettings()
+        {
+            if (string.IsNullOrEmpty(OLModDir))
+            {
+                Debug.Log("olmod directory unknown " + Environment.GetEnvironmentVariable("OLMODDIR") + " path " + Environment.GetEnvironmentVariable("PATH"));
+                return;
+            }
+            string settingsFilename = Path.Combine(OLModDir, "olmodsettings.json");
+            try
+            {
+                Settings = JObject.Parse(File.ReadAllText(settingsFilename));
+                Debug.Log("olmod settings loaded from " + settingsFilename);
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("olmod settings loading failed " + ex.Message + " " + settingsFilename);
+            }
+        }
 
         public static void Init()
         {
+            OLModDir = Environment.GetEnvironmentVariable("OLMODDIR");
             NoDownload = Core.GameMod.FindArg("-nodownload");
+            Settings = new JObject();
+            LoadSettings();
         }
     }
 }
