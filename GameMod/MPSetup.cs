640c1c05d5616833a6301ad53b4c33cfdf426b99
Arne de Bruijn <mail@arnedebruijn.nl>
2019-08-14 22:44:13 +0200

    Store olmod settings in separate file

--- GameMod/MPSetup.cs
+++ GameMod/MPSetup.cs
@@ -1,6 +1,7 @@
 ﻿using Harmony;
 using Overload;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -100,16 +101,107 @@ namespace GameMod
         }
     }
 
+    static class ModPrefs
+    {
+        private static Hashtable m_prefs_hashtable = new Hashtable();
+        private static string m_serialized_data = string.Empty;
+
+        private static object oldPrefs, oldData;
+
+        private static void SwapPre()
+        {
+            var fieldPrefs = typeof(GamePreferences).GetField("m_prefs_hashtable", BindingFlags.NonPublic | BindingFlags.Static);
+            var fieldData = typeof(GamePreferences).GetField("m_serialized_data", BindingFlags.NonPublic | BindingFlags.Static);
+            oldPrefs = fieldPrefs.GetValue(null);
+            oldData = fieldData.GetValue(null);
+            fieldPrefs.SetValue(null, m_prefs_hashtable);
+            fieldData.SetValue(null, m_serialized_data);
+        }
+
+        private static void SwapPost()
+        {
+            var fieldPrefs = typeof(GamePreferences).GetField("m_prefs_hashtable", BindingFlags.NonPublic | BindingFlags.Static);
+            var fieldData = typeof(GamePreferences).GetField("m_serialized_data", BindingFlags.NonPublic | BindingFlags.Static);
+            m_prefs_hashtable = (Hashtable)fieldPrefs.GetValue(null);
+            m_serialized_data = (string)fieldData.GetValue(null);
+            fieldPrefs.SetValue(null, oldPrefs);
+            fieldData.SetValue(null, oldData);
+            oldPrefs = null;
+            oldData = null;
+        }
+
+        public static bool Load(string filename)
+        {
+            SwapPre();
+            GamePreferences.Load(filename);
+            SwapPost();
+            return m_serialized_data != null;
+        }
+
+        public static void Flush(string filename)
+        {
+            SwapPre();
+            GamePreferences.Flush(filename);
+            SwapPost();
+        }
+
+        public static void DeleteAll()
+        {
+            m_prefs_hashtable.Clear();
+        }
+
+        public static int GetInt(string key, int defaultValue)
+        {
+            if (m_prefs_hashtable.ContainsKey(key))
+            {
+                return (int)m_prefs_hashtable[key];
+            }
+            m_prefs_hashtable.Add(key, defaultValue);
+            return defaultValue;
+        }
+
+        public static bool GetBool(string key, bool defaultValue)
+        {
+            if (m_prefs_hashtable.ContainsKey(key))
+            {
+                return (bool)m_prefs_hashtable[key];
+            }
+            m_prefs_hashtable.Add(key, defaultValue);
+            return defaultValue;
+        }
+
+        public static void SetInt(string key, int value)
+        {
+            m_prefs_hashtable[key] = value;
+        }
+
+        public static void SetBool(string key, bool value)
+        {
+            m_prefs_hashtable[key] = value;
+        }
+    }
+
     [HarmonyPatch(typeof(MenuManager), "LoadPreferences")]
     class MPSetupLoad
     {
-        static void Postfix()
+        static void Postfix(string filename)
         {
-            MPTeams.MenuManagerTeamCount = MenuManager.LocalGetInt("MP_PM_TEAM_COUNT", MPTeams.MenuManagerTeamCount);
-            MPJoinInProgress.MenuManagerEnabled = MenuManager.LocalGetBool("MP_PM_JIP", MPJoinInProgress.MenuManagerEnabled);
-            RearView.MPMenuManagerEnabled = MenuManager.LocalGetBool("MP_PM_REARVIEW", RearView.MPMenuManagerEnabled);
-            Console.KeyEnabled = MenuManager.LocalGetBool("O_CONSOLE_KEY", Console.KeyEnabled);
-            Console.CustomUIColor = MenuManager.LocalGetInt("O_CUSTOM_UI_COLOR", Console.CustomUIColor);
+            if (ModPrefs.Load(filename + "mod"))
+            {
+                MPTeams.MenuManagerTeamCount = ModPrefs.GetInt("MP_PM_TEAM_COUNT", MPTeams.MenuManagerTeamCount);
+                MPJoinInProgress.MenuManagerEnabled = ModPrefs.GetBool("MP_PM_JIP", MPJoinInProgress.MenuManagerEnabled);
+                RearView.MPMenuManagerEnabled = ModPrefs.GetBool("MP_PM_REARVIEW", RearView.MPMenuManagerEnabled);
+                Console.KeyEnabled = ModPrefs.GetBool("O_CONSOLE_KEY", Console.KeyEnabled);
+                Console.CustomUIColor = ModPrefs.GetInt("O_CUSTOM_UI_COLOR", Console.CustomUIColor);
+            }
+            else
+            {
+                MPTeams.MenuManagerTeamCount = MenuManager.LocalGetInt("MP_PM_TEAM_COUNT", MPTeams.MenuManagerTeamCount);
+                MPJoinInProgress.MenuManagerEnabled = MenuManager.LocalGetBool("MP_PM_JIP", MPJoinInProgress.MenuManagerEnabled);
+                RearView.MPMenuManagerEnabled = MenuManager.LocalGetBool("MP_PM_REARVIEW", RearView.MPMenuManagerEnabled);
+                Console.KeyEnabled = MenuManager.LocalGetBool("O_CONSOLE_KEY", Console.KeyEnabled);
+                Console.CustomUIColor = MenuManager.LocalGetInt("O_CUSTOM_UI_COLOR", Console.CustomUIColor);
+            }
         }
     }
 
@@ -122,17 +214,19 @@ namespace GameMod
         {
             if (MenuManager.LocalGetInt("PS_XP2", 0) == 0 && lastXP > 0)
                 MenuManager.LocalSetInt("PS_XP2", lastXP);
-
-            MenuManager.LocalSetInt("MP_PM_TEAM_COUNT", MPTeams.MenuManagerTeamCount);
-            MenuManager.LocalSetBool("MP_PM_JIP", MPJoinInProgress.MenuManagerEnabled);
-            MenuManager.LocalSetBool("MP_PM_REARVIEW", RearView.MPMenuManagerEnabled);
-            MenuManager.LocalSetBool("O_CONSOLE_KEY", Console.KeyEnabled);
-            MenuManager.LocalSetInt("O_CUSTOM_UI_COLOR", Console.CustomUIColor);
         }
 
-        private static void Prefix()
+        private static void Prefix(string filename)
         {
             lastXP = MenuManager.LocalGetInt("PS_XP2", 0);
+
+            ModPrefs.DeleteAll();
+            ModPrefs.SetInt("MP_PM_TEAM_COUNT", MPTeams.MenuManagerTeamCount);
+            ModPrefs.SetBool("MP_PM_JIP", MPJoinInProgress.MenuManagerEnabled);
+            ModPrefs.SetBool("MP_PM_REARVIEW", RearView.MPMenuManagerEnabled);
+            ModPrefs.SetBool("O_CONSOLE_KEY", Console.KeyEnabled);
+            ModPrefs.SetInt("O_CUSTOM_UI_COLOR", Console.CustomUIColor);
+            ModPrefs.Flush(filename + "mod");
         }
 
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
