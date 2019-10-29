f77d85aa7220ebf4b77f6a5a3030d20e42fdff32
Arne de Bruijn <mail@arnedebruijn.nl>
2019-10-29 21:42:02 +0100

    Allow specifing server port on command line

---
+++ GameMod/ServerPort.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+using Overload;
+
+namespace GameMod
+{
+    [HarmonyPatch(typeof(Server), "Listen")]
+    class ServerPort
+    {
+        static int PortArg = 0;
+
+        private static bool Prepare(){
+            if (!Core.GameMod.FindArgVal("-port", out string arg) || !int.TryParse(arg, out int val))
+                return false;
+            PortArg = val;
+            return true;
+        }
+
+        private static void Prefix(ref int port)
+        {
+            if (port == 0)
+                port = PortArg;
+        }
+    }
+}
