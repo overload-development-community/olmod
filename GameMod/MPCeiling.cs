643f8843a093894f00a7248fe52ddc64eaba32a6
Arne de Bruijn <mail@arnedebruijn.nl>
2019-11-07 21:42:49 +0100

    Prevent items from moving beyond ceiling (by terminal)

---
+++ GameMod/MPCeiling.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using UnityEngine;
+
+// by terminal
+namespace GameMod
+{
+    // Prevent items from escaping through invisible ceilings on non-monsterball maps.
+    [HarmonyPatch(typeof(NetworkMatch), "StartPlaying")]
+    internal class MPItemCollideWithLayer31
+    {
+        private static void Postfix()
+        {
+            var isMonsterball = NetworkMatch.GetMode() == MatchMode.MONSTERBALL;
+            Physics.IgnoreLayerCollision(31, (int)Overload.UnityObjectLayers.ITEMS, isMonsterball); 
+            Physics.IgnoreLayerCollision(31, (int)Overload.UnityObjectLayers.ITEM_LEVEL, isMonsterball);
+        }
+    }
+}
