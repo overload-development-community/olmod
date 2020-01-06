e535c46928feab5fb7a0a9434b5fa8a2b1abf809
Arne de Bruijn <mail@arnedebruijn.nl>
2020-01-06 20:47:49 +0100

    Disable super alarm in level without super spawn (serverside)

---
+++ GameMod/MPSuperCheck.cs
@@ -0,0 +1,19 @@
+﻿using Harmony;
+using Overload;
+using System.Linq;
+
+// only start super spawn timer if super spawn actually exists in the level
+namespace GameMod
+{
+    [HarmonyPatch(typeof(NetworkMatch), "SetSpawnSuperTimer")]
+    class MPSuperCheck
+    {
+        static bool Prefix(ref float ___m_spawn_super_timer)
+        {
+            if (GameManager.m_level_data.m_item_spawn_points.Any(x => x.multiplayer_team_association_mask == 1)) // 1 -> is super
+                return true;
+            ___m_spawn_super_timer = -1f;
+            return false;
+        }
+    }
+}
