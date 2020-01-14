a0440798e45194187f7b5eedda69af86858ad355
Arne de Bruijn <mail@arnedebruijn.nl>
2020-01-14 18:07:28 +0100

    Version 0.2.7.91

--- GameMod/GameMod.cs
+++ GameMod/GameMod.cs
@@ -11,7 +11,7 @@ namespace GameMod.Core
 {
     public class GameMod
     {
-        public static readonly string Version = "olmod 0.2.7.90";
+        public static readonly string Version = "olmod 0.2.7.91";
 
         internal static void Initialize()
         {
