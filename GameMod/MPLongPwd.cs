5977668d050eab2cec7d0cc910f63699f61fef2e
Arne de Bruijn <mail@arnedebruijn.nl>
2020-02-11 19:14:24 +0100

    Allow long MP password, by Tobias

---
+++ GameMod/MPLongPwd.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+using Overload;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+// by Tobias
+namespace GameMod
+{
+    [HarmonyPatch(typeof(MenuManager), "ProcessInputField")]
+    class MPLongPwdProcessInputField
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            foreach (var code in codes)
+            {
+                if (code.opcode == OpCodes.Ldc_I4_S && (sbyte)code.operand == 16)
+                {
+                    code.operand = 127;
+                }
+                yield return code;
+            }
+        }
+    }
+}
