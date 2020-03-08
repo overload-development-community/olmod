eb0cb529cb1900b6d9496fcefda80a6326210674
Arne de Bruijn <mail@arnedebruijn.nl>
2020-03-08 19:46:01 +0100

    olmod.sh: add experimental osx support

--- linux/olmod.sh
+++ linux/olmod.sh
@@ -5,4 +5,10 @@ for arg; do
   if [ "$next_dir" = "0" ]; then OLDIR="$arg"; fi
   [ "$arg" = "-gamedir" ]; next_dir="$?"
 done
+ISMAC=""
+[ -n "$OSTYPE" ] && if [[ "$OSTYPE" == "darwin"* ]]; then ISMAC="1"; fi
+if [ -n "$ISMAC" ]; then
+DYLD_INSERT_LIBRARIES="$OLMODDIR/olmod.dylib" exec "$OLDIR/Overload.app/Contents/MacOS/Overload" "$@"
+else
 LD_PRELOAD="$OLMODDIR/olmod.so" exec "$OLDIR/Overload.x86_64" "$@"
+fi
