6e280882fb9fbe002e825383195a3d7fbf844b04
Ronald M. Clifford <roncli@roncli.com>
2021-03-10 11:21:42 -0800

    Prevent a powerup being picked up by 2 pilots at the same time.
    Fixes #81.

---
+++ GameMod/MPNoDupes.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Harmony;
+using Overload;
+
+namespace GameMod {
+    /// <summary>
+    /// Prevents stock items from being picked up by multiple pilots in the same frame.
+    /// </summary>
+    [HarmonyPatch(typeof(Item), "OnTriggerEnter")]
+    class MPNoDupes_OnTriggerEnter {
+        public static bool Prefix(Item __instance) {
+            return __instance.m_type != ItemType.NONE;
+        }
+
+        public static void SetItemTypeToNoneAndDestroy(Item item) {
+            item.m_type = ItemType.NONE;
+            UnityEngine.Object.Destroy(item.c_go);
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes) {
+            var ldfldCount = 0;
+
+            foreach (var code in codes) {
+                if (code.opcode == OpCodes.Ldfld) {
+                    ldfldCount++;
+
+                    if (ldfldCount == 39) {
+                        continue;
+                    }
+                }
+
+                if (code.opcode == OpCodes.Call && ((MethodInfo)code.operand).Name == "Destroy") {
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(MPNoDupes_OnTriggerEnter), "SetItemTypeToNoneAndDestroy"));
+                    continue;
+                }
+
+                yield return code;
+            }
+        }
+    }
+}
