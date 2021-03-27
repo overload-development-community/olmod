7dac6db0c904925cfa83fe9c2ba3445d6de077d7
Ronald M. Clifford <roncli@roncli.com>
2021-03-27 12:42:29 -0700

    Don't spawn a cloak super powerup when always cloaked is on.

--- GameMod/MPAlwaysCloaked.cs
+++ GameMod/MPAlwaysCloaked.cs
@@ -6,6 +6,9 @@ namespace GameMod {
         public static bool Enabled { get; set; } = false;
     }
 
+    /// <summary>
+    /// Make ships cloaked on spawn when mod enabled.
+    /// </summary>
     [HarmonyPatch(typeof(Player), "Update")]
     class MPAlwaysCloaked_Update {
         private static void Postfix(Player __instance) {
@@ -13,6 +16,24 @@ namespace GameMod {
                 __instance.Networkm_cloaked = true;
                 __instance.CallRpcActivateCloak(float.MaxValue);
                 __instance.CallTargetAddHUDMessage(__instance.connectionToClient, Loc.LSN("EVERYONE IS ALWAYS CLOAKED!"), -1, true);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Disallow cloak from spawning when mod enabled.
+    /// </summary>
+    [HarmonyPatch(typeof(NetworkMatch), "RandomAllowedSuperSpawn")]
+    class MPAlwaysCloaked_RandomAllowedSuperSpawn {
+        private static void Prefix() {
+            if (MPAlwaysCloaked.Enabled) {
+                for (int index = 0; index < RobotManager.m_multiplayer_spawnable_supers.Count; index++) {
+                    var super = RobotManager.m_multiplayer_spawnable_supers[index];
+                    if (super.type == (int)SuperType.CLOAK) {
+                        super.percent = 0;
+                        RobotManager.m_multiplayer_spawnable_supers[index] = super;
+                    }
+                }
             }
         }
     }
