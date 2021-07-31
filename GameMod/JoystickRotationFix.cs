4a7a5c2b2ccd4d2b1f2aaa48002f9d6e89541b93
lu <denda98@gmx.de>
2021-07-31 14:09:23 +0200

    Clean up rotation fix debugging statements

--- GameMod/JoystickRotationFix.cs
+++ GameMod/JoystickRotationFix.cs
@@ -18,52 +18,9 @@ namespace GameMod
     class JoystickRotationFix
     {
 
-        public static bool alt_turn_ramp_mode = false;          // defines which behaviour the local client wants to use, gets loaded/saved through MPSetup.cs in .xprefsmod
-        public static Dictionary<uint, int> client_settings = new Dictionary<uint, int>();    // used to store the turn ramp mode setting of the active players on the server side
-        public static bool server_support = true;               // indicates wether the current server supports the changed behaviour, has to be true outside of games to make the ui option accessible
-
-
-
-        [HarmonyPatch(typeof(GameManager), "Start")]
-        internal class CommandsAndInitialisationPatch10
-        {
-            private static void Postfix(GameManager __instance)
-            {
-                uConsole.RegisterCommand("debuginput", "", new uConsole.DebugCommand(CommandsAndInitialisationPatch10.CmdToggleDebugInput));
-            }
-
-            private static void CmdToggleDebugInput()
-            {
-                JoystickCurveEditor.DebugOutput.show = !JoystickCurveEditor.DebugOutput.show;
-            }
-        }
-
-
-
-        // Debug 
-        /*
-        [HarmonyPatch(typeof(Player), "CmdSendFullChat")]
-        internal class JoystickRotationFix_Debug_CmdSendFullChat
-        {
-            static void Postfix(int sender_connection_id, string sender_name, MpTeam sender_team, string msg)
-            {
-                if (msg.ToString().ToUpper().StartsWith("SHOW_JFS"))
-                {
-                    string msg3 = "empty settings";
-                    if (client_settings != null)
-                    {
-                        msg3 = "";
-                        foreach (KeyValuePair<uint, int> entry in client_settings)
-                        {
-                            msg3 += "  Key:" + entry.Key + "  Value:" + entry.Value;
-                        }
-                    }
-                    LobbyChatMessage msg2 = new LobbyChatMessage(sender_connection_id, "SERVER", MpTeam.ANARCHY, msg3, false);
-                    NetworkServer.SendToAll(75, msg2);
-                }
-            }
-        }*/
-
+        public static bool alt_turn_ramp_mode = false;                                      // defines which behaviour the local player uses, gets loaded/saved through MPSetup.cs in .xprefsmod
+        public static Dictionary<uint, int> client_settings = new Dictionary<uint, int>();  // used to store the turn ramp mode setting of the active players on the server
+        public static bool server_support = true;                                           // indicates wether the current server supports the changed behaviour, has to be true outside of games to make the ui option accessible
 
 
         [HarmonyPatch(typeof(PlayerShip), "FixedUpdateProcessControlsInternal")]
@@ -75,12 +32,13 @@ namespace GameMod
             {
                 _inst = __instance;
             }
-
+            // resets num20 to cc_turn_vec.y, num21 to cc_turn_vec.x and multiplies them with the selected PlayerShip.m_ramp_max[] if the linear behaviour is 
+            // selected and supported on the server
             static IEnumerable<CodeInstruction> Transpiler(ILGenerator ilGen, IEnumerable<CodeInstruction> instructions)
             {
                 var playerShip_c_player_Field = AccessTools.Field(typeof(PlayerShip), "c_player");
                 var player_cc_turn_vec_Field = AccessTools.Field(typeof(Player), "cc_turn_vec");
-                var joystickRotationFix_MaybeResetVector_Method = AccessTools.Method(typeof(JoystickRotationFix_FixedUpdateProcessControlsInternal), "MaybeResetVector");
+                var joystickRotationFix_MaybeResetToInput_Method = AccessTools.Method(typeof(JoystickRotationFix_FixedUpdateProcessControlsInternal), "MaybeResetToInput");
                 var joystickRotationFix_MaybeScaleUpRotation_Method = AccessTools.Method(typeof(JoystickRotationFix_FixedUpdateProcessControlsInternal), "MaybeScaleUpRotation");
 
                 var codes = new List<CodeInstruction>(instructions);
@@ -97,14 +55,14 @@ namespace GameMod
                                 new CodeInstruction(OpCodes.Ldfld, playerShip_c_player_Field),
                                 new CodeInstruction(OpCodes.Ldflda, player_cc_turn_vec_Field),
                                 new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(UnityEngine.Vector3), "y")),
-                                new CodeInstruction(OpCodes.Call, joystickRotationFix_MaybeResetVector_Method)
+                                new CodeInstruction(OpCodes.Call, joystickRotationFix_MaybeResetToInput_Method)
                             };
                             var resetNum21 = new[] {
                                 new CodeInstruction(OpCodes.Ldarg_0),
                                 new CodeInstruction(OpCodes.Ldfld, playerShip_c_player_Field),
                                 new CodeInstruction(OpCodes.Ldflda, player_cc_turn_vec_Field),
                                 new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(UnityEngine.Vector3), "x")),
-                                new CodeInstruction(OpCodes.Call, joystickRotationFix_MaybeResetVector_Method)
+                                new CodeInstruction(OpCodes.Call, joystickRotationFix_MaybeResetToInput_Method)
                             };
                             var adjustScaling = new[] {
                                 new CodeInstruction(OpCodes.Call, joystickRotationFix_MaybeScaleUpRotation_Method),
@@ -122,14 +80,13 @@ namespace GameMod
 
             }
 
-            public static float MaybeResetVector(float original, float changed)
+            public static float MaybeResetToInput(float original, float changed)
             {
                 return MaybeChangeRotation() ? changed : original;
             }
 
             public static float MaybeScaleUpRotation()
             {
-                //Vmax = F / (mass * drag)  player.rigidbody.drag = 5.5f
                 return MaybeChangeRotation() ? PlayerShip.m_ramp_max[_inst.c_player.m_player_control_options.opt_joy_ramp] : 1f;
             }
 
@@ -166,9 +123,6 @@ namespace GameMod
             }
         }
 
-        // reset the stored client settings after each match and make sure that the menu option on the clients is enabled
-
-
 
         /// ///////////////////////////////////// SERVER ///////////////////////////////////// ///
         [HarmonyPatch(typeof(Server), "RegisterHandlers")]
@@ -201,7 +155,7 @@ namespace GameMod
 
 
         /// ///////////////////////////////////// CLIENT ///////////////////////////////////// ///
-        [HarmonyPatch(typeof(Client), "OnMatchStart")] //SendPlayerLoadoutToServer
+        [HarmonyPatch(typeof(Client), "OnMatchStart")] // m_local_player.netId will return 0 or an old netid if this gets called earlier than OnMatchStart
         class JoystickRotationFix_SendPlayerLoadoutToServer
         {
             private static void Postfix()
@@ -212,13 +166,11 @@ namespace GameMod
                     Debug.Log("JoystickRamping_SendPlayerLoadoutToServer: no client?");
                     return;
                 }
-                NetworkIdentity ni = GameManager.m_local_player.GetComponent<NetworkIdentity>();
-                uConsole.Log("----------> just sent " + GameManager.m_local_player.netId.Value + ", " + alt_turn_ramp_mode + "   " + ni.netId.Value + "  <----------");
                 Client.GetClient().Send(MessageTypes.MsgSetTurnRampMode,
                     new SetTurnRampModeMessage
                     {
                         mode = alt_turn_ramp_mode ? 1 : 0,
-                        netId = ni.netId.Value
+                        netId = GameManager.m_local_player.netId.Value
                     });
             }
         }
@@ -253,7 +205,7 @@ namespace GameMod
                     Debug.Log("JoystickRamping.server_support didnt reset properly");
                 }
                 position.y += 62f;
-                uie.SelectAndDrawStringOptionItem(Loc.LS("TURN RAMP MODE"), position, 0, alt_turn_ramp_mode ? "LINEAR" : "DEFAULT", Loc.LS(""), 1.5f, !server_support);
+                uie.SelectAndDrawStringOptionItem(Loc.LS("MAX TURN RAMP MODE"), position, 0, alt_turn_ramp_mode ? "LINEAR" : "DEFAULT", "LINEAR ADDS THE MAX TURN RAMPING SPEED LINEARLY ALONG THE INPUT [KB/JOYSTICK ONLY]", 1.5f, !server_support);
             }
 
             static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
@@ -292,7 +244,6 @@ namespace GameMod
                         Debug.Log("JoystickRamping_ControlsOptionsUpdate: no client?");
                         return;
                     }
-                    //Debug.Log("Just sent the updated flag to the server: current: " + (alt_turn_ramp_mode ? "LINEAR" : "DEFAULT"));
                     Client.GetClient().Send(MessageTypes.MsgSetTurnRampMode, new SetTurnRampModeMessage { mode = alt_turn_ramp_mode ? 1 : 0, netId = GameManager.m_local_player.netId.Value });
                 }
             }
