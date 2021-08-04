ce78f8e97c86cfdad09beae8ea6f1cfec7ee974b
Arne de Bruijn <mail@arnedebruijn.nl>
2021-08-04 13:00:56 +0200

    Save mission progress with olmod

--- GameMod/DisableTimeCheat.cs
+++ GameMod/DisableTimeCheat.cs
@@ -1,5 +1,8 @@
 ﻿using HarmonyLib;
 using Overload;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace GameMod
 {
@@ -9,6 +12,30 @@ namespace GameMod
         static void Prefix()
         {
             GameManager.m_gm.DisableTimeCheatingDetector();
+            GameManager.m_cheating_type = "olmod";
+        }
+    }
+
+    [HarmonyPatch(typeof(PilotManager), "Save")]
+    class PilotSave
+    {
+        public static bool OtherDetected()
+        {
+            return GameManager.m_cheating_detected && GameManager.m_cheating_type != "olmod";
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> cs)
+        {
+            foreach (var c in cs)
+            {
+                if (c.opcode == OpCodes.Ldsfld && ((FieldInfo)c.operand).Name == "m_cheating_detected")
+                {
+                    yield return new CodeInstruction(OpCodes.Call,
+                        typeof(PilotSave).GetMethod("OtherDetected", BindingFlags.Public | BindingFlags.Static));
+                    continue;
+                }
+                yield return c;
+            }
         }
     }
 }
