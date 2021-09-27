263fed1391bf33f3ec1f837fd3c42e50934b8af0
Ronald M. Clifford <roncli@roncli.com>
2021-09-26 21:31:02 -0700

    CPU usage fixes, don't consider doors to break line of sight for spawn algorithm.

--- GameMod/MPRespawn.cs
+++ GameMod/MPRespawn.cs
@@ -97,10 +97,7 @@ namespace GameMod {
                     var position2 = spawn2.position;
                     var segnum2 = GameManager.m_level_data.FindSegmentContainingWorldPosition(position2);
 
-                    var distance = MPRespawn_ChooseSpawnPoint.FindShortestPath(segnum, segnum2, -1, 9999f);
-                    if (distance <= 0f || distance >= 9999f) {
-                        distance = RUtility.FindVec3Distance(position - position2);
-                    }
+                    var distance = RUtility.FindVec3Distance(position - position2);
 
                     MPRespawn.spawnPointDistances[spawn1].Add(spawn2, distance);
                 }
@@ -156,7 +153,6 @@ namespace GameMod {
         }
 
         private static Dictionary<LevelData.SpawnPoint, DateTime> lastRespawn = new Dictionary<LevelData.SpawnPoint, DateTime>();
-        private static MethodInfo _UIManager_VisibilityRaycast_Method = AccessTools.Method(typeof(UIManager), "VisibilityRaycast");
 
         internal static List<Quaternion> m_player_rot = new List<Quaternion>();
 
@@ -190,14 +186,8 @@ namespace GameMod {
             for (int i = 0; i < candidates.Count; i++) {
                 var dist = new List<float>();
                 var position = candidates[i].position;
-                var segnum = GameManager.m_level_data.FindSegmentContainingWorldPosition(position);
                 for (int j = 0; j < playerPositions.Count; j++) {
-                    var playerSegnum = GameManager.m_level_data.FindSegmentContainingWorldPosition(playerPositions[j]);
-
-                    var distance = FindShortestPath(segnum, playerSegnum, -1, 9999f);
-                    if (distance <= 0f || distance >= 9999f) {
-                        distance = RUtility.FindVec3Distance(position - playerPositions[j]);
-                    }
+                    var distance = RUtility.FindVec3Distance(position - playerPositions[j]);
 
                     if (distance > max) {
                         max = distance;
@@ -263,13 +253,13 @@ namespace GameMod {
                 if (team != playerTeams[i] || NetworkMatch.GetMode() == MatchMode.ANARCHY) {
                     closest = Math.Min(closest, dist);
 
-                    if ((bool)_UIManager_VisibilityRaycast_Method.Invoke(null, new object[] { spawnPoint.position, vector, vectorDist })) {
+                    if (VisibilityRaycast(spawnPoint.position, vector, vectorDist)) {
                         var angle = Math.Min(Vector3.Angle(playerPositions[i] - spawnPoint.position, spawnPoint.orientation * Vector3.forward), Vector3.Angle(spawnPoint.position - playerPositions[i], m_player_rot[i] * Vector3.forward));
                         leastLoS = Math.Min(leastLoS, angle);
                         seesEnemy = true;
                     }
                 } else {
-                    if (dist <= 20f && (bool)_UIManager_VisibilityRaycast_Method.Invoke(null, new object[] { spawnPoint.position, vector, vectorDist })) {
+                    if (dist <= 30f && VisibilityRaycast(spawnPoint.position, vector, vectorDist)) {
                         seesTeammate = true;
                         closestTeammate = Math.Min(dist, closestTeammate);
                     }
@@ -278,6 +268,10 @@ namespace GameMod {
                 if (dist < 15f) {
                     scale = (float)Math.Pow(Math.Min(scale, dist / 15f), 2);
                 }
+            }
+
+            if (closest > 80f) {
+                scale *= 80f / closest;
             }
 
             // Score the spawn point.
@@ -296,111 +290,11 @@ namespace GameMod {
             return score;
         }
 
-        private static MethodInfo _Pathfinding_LegalEntryPortal_Method = AccessTools.Method(typeof(Pathfinding), "LegalEntryPortal");
-        private static MethodInfo _Pathfinding_MarkAllSegments_Method = AccessTools.Method(typeof(Pathfinding), "MarkAllSegments");
-        private static MethodInfo _Pathfinding_StorePath1_Method = AccessTools.Method(typeof(Pathfinding), "StorePath1");
-        private static FieldInfo _Pathfinding_m_level_data_Field = typeof(Pathfinding).GetField("m_level_data", BindingFlags.NonPublic | BindingFlags.Static);
-        private static FieldInfo _Pathfinding_m_path_nodes_Field = typeof(Pathfinding).GetField("m_PathNodes", BindingFlags.NonPublic | BindingFlags.Static);
-        private static FieldInfo _Pathfinding_m_segment_buffer_Field = typeof(Pathfinding).GetField("m_SegmentBuffer", BindingFlags.NonPublic | BindingFlags.Static);
-
-        public static float FindShortestPath(int start_seg, int end_seg, int avoid_seg, float max_distance) {
-            int num;
-            int num2 = 1;
-            int num3 = end_seg;
-            float path_distance = 0f;
-            if (start_seg == -1 || end_seg == -1) {
-                return 9999f;
-            }
-            if (_Pathfinding_m_level_data_Field.GetValue(null) == null) {
-                Debug.Log(Time.frameCount + ": uninitialized level data.");
-                return 9999f;
-            }
-            Vector3 center = Pathfinding.Segments[num3].Center;
-            _Pathfinding_MarkAllSegments_Method.Invoke(null, new object[] { });
-            if (avoid_seg != -1) {
-                ((bool[])_Pathfinding_m_segment_buffer_Field.GetValue(null))[avoid_seg] = true;
-            }
-
-            var m_path_nodes = ((Pathfinding.PathNode[])_Pathfinding_m_path_nodes_Field.GetValue(null));
-
-            m_path_nodes[0].segnum = num3;
-            m_path_nodes[0].distance = 0f;
-            m_path_nodes[0].parent = -1;
-            ((bool[])_Pathfinding_m_segment_buffer_Field.GetValue(null))[num3] = true;
-            num = 1;
-            int num4 = 0;
-            Vector3 vector = default(Vector3);
-            int num10;
-            while (true) {
-                int num5;
-                if (num4 < num2 + 1) {
-                    if (m_path_nodes[num4].segnum != -1) {
-                        center = Pathfinding.Segments[m_path_nodes[num4].segnum].Center;
-                        num3 = m_path_nodes[num4].segnum;
-                        num5 = 0;
-                        for (int i = 0; i < 6; i++) {
-                            SegmentData segmentData = Pathfinding.Segments[num3];
-                            int num6 = segmentData.Portals[i];
-                            if (num6 != -1 || segmentData.WarpDestinationSegs[i] != -1) {
-                                int num7;
-                                if (segmentData.WarpDestinationSegs[i] != -1) {
-                                    num7 = segmentData.WarpDestinationSegs[i];
-                                } else {
-                                    PortalData portalData = Pathfinding.Portals[num6];
-                                    if ((segmentData.DecalFlags & (uint)(1 << i)) != 0 || (segmentData.Pathfinding == PathfindingType.GUIDEBOT_ONLY) || !(bool)_Pathfinding_LegalEntryPortal_Method.Invoke(null, new object[] { num3, num6, true })) {
-                                        continue;
-                                    }
-                                    num7 = ((portalData.MasterSegmentIndex != num3) ? portalData.MasterSegmentIndex : portalData.SlaveSegmentIndex);
-                                }
-                                if (!((bool[])_Pathfinding_m_segment_buffer_Field.GetValue(null))[num7]) {
-                                    vector.x = Pathfinding.Segments[num7].Center.x - center.x;
-                                    vector.y = Pathfinding.Segments[num7].Center.y - center.y;
-                                    vector.z = Pathfinding.Segments[num7].Center.z - center.z;
-                                    float num8 = vector.x * vector.x + vector.y * vector.y + vector.z * vector.z;
-                                    float num9 = m_path_nodes[num4].distance + (float)Math.Sqrt(num8);
-                                    if (num9 < max_distance) {
-                                        m_path_nodes[num + num5].distance = num9;
-                                        m_path_nodes[num + num5].segnum = num7;
-                                        m_path_nodes[num + num5].parent = num4;
-                                        ((bool[])_Pathfinding_m_segment_buffer_Field.GetValue(null))[num7] = true;
-                                        num5++;
-                                        num2 = num + num5;
-                                    } else {
-                                        m_path_nodes[num + num5].segnum = -1;
-                                    }
-                                } else {
-                                    m_path_nodes[num + num5].segnum = -1;
-                                }
-                                if (num7 != start_seg) {
-                                    continue;
-                                }
-                                goto IL_0393;
-                            }
-                            m_path_nodes[num + num5].segnum = -1;
-                        }
-                        num += num5;
-                    }
-                    num4++;
-                    continue;
-                }
-                num10 = -1;
-                break;
-            IL_0393:
-                num10 = num + num5 - 1;
-                path_distance = m_path_nodes[num10].distance;
-                break;
-            }
-            if (num4 > num2) {
-                return 9999f;
-            }
-            if (num10 == -1) {
-                num10 = -1;
-            }
-            int path_length = (int)_Pathfinding_StorePath1_Method.Invoke(null, new object[] { null, num10, start_seg, end_seg });
-            if (path_length == -1) {
-                return 9999f;
-            }
-            return path_distance;
+        private static bool VisibilityRaycast(Vector3 pos, Vector3 td_vec, float dist) {
+            int layerMask = 67125248;
+            RaycastHit hitInfo;
+            bool flag = Physics.Raycast(pos, td_vec, out hitInfo, dist, layerMask);
+            return !flag;
         }
     }
 }
