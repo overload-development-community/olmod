357f9586bd46baeedeaab318b716f3abe5a300a8
Ronald M. Clifford <roncli@roncli.com>
2021-11-08 15:15:39 -0800

    Add -missionpath <dir> argument that loads missions from an arbitrary directory.

--- GameMod/AddOnLevelSort.cs
+++ GameMod/AddOnLevelSort.cs
@@ -2,6 +2,8 @@
 using Overload;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
@@ -17,12 +19,29 @@ namespace GameMod
     [HarmonyPatch(typeof(GameManager), "InitializeMissionList")]
     class AddOnLevelSort
     {
+        private static FieldInfo _GameManager_m_mission_list_Field = typeof(GameManager).GetField("m_mission_list", BindingFlags.NonPublic | BindingFlags.Static);
+        private static MethodInfo _GameManager_ScanForLevels_Method = typeof(GameManager).GetMethod("ScanForLevels", BindingFlags.NonPublic | BindingFlags.Instance);
         private static FieldInfo _Mission_Levels_Field = typeof(Mission).GetField("Levels", BindingFlags.NonPublic | BindingFlags.Instance);
         private static PropertyInfo _LevelInfo_LevelNum_Property = typeof(LevelInfo).GetProperty("LevelNum");
-        static void Postfix()
+        static void Postfix(GameManager __instance)
         {
             try
             {
+                // Scan for levels in -missionpath directory
+                if (Core.GameMod.FindArgVal("-missionpath", out var path) && Directory.Exists(path)) {
+                    var lastMission = ((List<Mission>)_GameManager_m_mission_list_Field.GetValue(null)).LastOrDefault();
+
+                    int index = 0;
+
+                    if (lastMission != null) {
+                        index = lastMission.MissionIndex + 1;
+                    }
+
+                    var args = new object[] { path, index };
+                    _GameManager_ScanForLevels_Method.Invoke(__instance, args);
+                }
+
+                // Sort addon levels by DisplayName instead of arbitrary load
                 object __obj = _Mission_Levels_Field.GetValue(GameManager.MultiplayerMission);
                 List<LevelInfo> __m_mission_list = (List<LevelInfo>)__obj;
 
