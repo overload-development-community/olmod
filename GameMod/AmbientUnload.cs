6ae37b78bb911cdaf0c1906fb45076e171dff9b2
Arne de Bruijn <mail@arnedebruijn.nl>
2021-11-24 13:34:24 +0100

    Unload ambient sounds at level end
    This fixes missing custom ambient sounds when the level is restarted.

---
+++ GameMod/AmbientUnload.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace GameMod
+{
+    // Fix reloading custom ambient sounds by unloading them after the level ends
+    [HarmonyPatch(typeof(UnityAudio), "KillAllSounds")]
+    class AmbientUnload
+    {
+        static void Postfix(UnityAudio __instance) {
+            foreach (AudioSource src in __instance.m_ambient_sources)
+                src.clip.UnloadAudioData();
+        }
+    }
+}
