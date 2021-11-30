e7d79fda7308449f69dcec13c298d8a80390e35b
Tobias <tobiasksu@gmail.com>
2021-11-30 17:07:38 -0600

    Skip the portion in Player.UpdateInvul() where ship movement reduces your spawn invuln time

---
+++ GameMod/MPSpawnInvuln.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using Overload;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace GameMod
+{
+
+    // Skip the portion in Player.UpdateInvul() where ship movement reduces your invuln time
+    [HarmonyPatch(typeof(Player), "UpdateInvul")]
+    internal class MPSpawnInvuln_Player_UpdateInvul
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            int state = 0;
+            foreach (var code in codes)
+            {
+                if (code.opcode == OpCodes.Stloc_1)
+                {
+                    state++;
+                    if (state == 2)
+                        code.opcode = OpCodes.Pop;
+                }
+
+                yield return code;
+            }
+        }
+    }
+}
