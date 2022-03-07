9e72b6d3c25883cc745ec901d6ed21421f8defc8
Lukas Denda <34916038+luponix@users.noreply.github.com>
2022-03-07 17:26:48 +0100

    Disables the Profanity Filter

---
+++ GameMod/DisableProfanityFilter.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace GameMod
+{
+    class DisableProfanityFilter
+    {
+        /// <summary>
+        /// Author: luponix
+        /// Created: 2022-03-07
+        /// Removes the effects of the Profanity filter by returning the string immediatly
+        /// </summary>
+        [HarmonyPatch(typeof(StringParse), "ProfanityFilter")]
+        class DisableProfanityFilter_StringParse_ProfanityFilter
+        {
+            static bool Prefix(string s, ref string __result)
+            {
+                __result = s;
+                return false;
+            }
+        }
+    }
+}
