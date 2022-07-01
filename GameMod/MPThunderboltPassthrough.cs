da3ff4cc35b03a89042e1b0c348293ab9c3a3cc4
Lukas Denda <34916038+luponix@users.noreply.github.com>
2022-07-02 00:32:59 +0200

    Fixes Thunderbolt projectiles not dieing when hitting lava

--- GameMod/MPThunderboltPassthrough.cs
+++ GameMod/MPThunderboltPassthrough.cs
@@ -14,49 +14,6 @@ namespace GameMod
     {
 
 		public static bool isAllowed = false;
-
-		[HarmonyPatch(typeof(Projectile), "ProcessCollision")]
-		internal class MPClassic_Projectile_ProcessCollision
-		{
-			private static void ThunderboltExplode(Projectile proj, int layer)
-			{
-				if (isAllowed)
-				{
-					bool flag2 = layer != 11 && layer != 16;
-					if (flag2)
-					{
-						proj.m_alive = false;
-						proj.Explode(false);
-					}
-				}
-			}
-
-			private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
-			{
-				int state = 0;
-				foreach (CodeInstruction code in codes)
-				{
-					bool flag = code.opcode == OpCodes.Ldc_I4_S && (sbyte)code.operand == 16;
-					if (flag)
-					{
-						state = 1;
-					}
-					bool flag2 = state == 1 && code.opcode == OpCodes.Call && code.operand == AccessTools.Method(typeof(Projectile), "Explode", null, null);
-					if (flag2)
-					{
-						yield return code;
-						yield return new CodeInstruction(OpCodes.Ldarg_0, null);
-						yield return new CodeInstruction(OpCodes.Ldloc_2, null);
-						yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(MPClassic_Projectile_ProcessCollision), "ThunderboltExplode", null, null));
-					}
-					else
-					{
-						yield return code;
-					}
-				}
-				yield break;
-			}
-		}
 		
 		[HarmonyPatch(typeof(Projectile), "Explode")]
 		internal class MPClassic_Projectile_Explode
@@ -67,6 +24,60 @@ namespace GameMod
 			}
 		}
 
+		[HarmonyPatch(typeof(Projectile), "ProcessCollision")]
+		internal class MPWeaponBehavior_Projectile_ProcessCollision
+		{
+			private static void ThunderboltExplode(Projectile proj, int layer)
+			{
 
+				bool flag = layer == 11 || layer == 16;
+				if (!(flag & isAllowed))
+                {
+					proj.m_alive = false;
+					proj.Explode(!flag);
+				}
+			}
+
+			private static void FakeExplode(Projectile proj, int layer)
+			{
+
+			}
+
+			private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+			{
+				var projectile_m_alive_Field = AccessTools.Field(typeof(Projectile), "m_alive");
+
+				int state = 0;
+				foreach (CodeInstruction code in codes)
+				{
+					if (code.opcode == OpCodes.Ldc_I4_S && (sbyte)code.operand == 26)
+						state = 1;
+
+					if (state == 1 && code.opcode == OpCodes.Call && code.operand == AccessTools.Method(typeof(Projectile), "Explode", null, null))
+                    {
+						yield return new CodeInstruction(OpCodes.Ldarg_0);
+						yield return new CodeInstruction(OpCodes.Ldc_I4_0);
+						yield return new CodeInstruction(OpCodes.Stfld, projectile_m_alive_Field);
+						state = 2;
+                    }
+
+					if (code.opcode == OpCodes.Ldc_I4_S && (sbyte)code.operand == 16)
+						state = 3;
+
+					if (state == 3 && code.opcode == OpCodes.Call && code.operand == AccessTools.Method(typeof(Projectile), "Explode", null, null))
+					{
+						List<Label> lbls = code.labels;
+						yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(MPWeaponBehavior_Projectile_ProcessCollision), "FakeExplode"));
+						yield return new CodeInstruction(OpCodes.Ldarg_0);
+						yield return new CodeInstruction(OpCodes.Ldloc_2);
+						yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(MPWeaponBehavior_Projectile_ProcessCollision), "ThunderboltExplode")) { labels = lbls };
+						state = 4;
+						continue;
+					}
+
+					yield return code;
+				}
+			}
+		}
 	}
 }
