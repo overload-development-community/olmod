ba4fb12d66f25f841fd3a11b4f9fbb3d674c18aa
Ronald M. Clifford <roncli@roncli.com>
2022-09-25 09:12:43 -0700

    Convert VSync.

--- GameMod/Patches/Overload/MenuManager.cs
+++ GameMod/Patches/Overload/MenuManager.cs
@@ -1,19 +1,22 @@
-﻿using HarmonyLib;
-using Overload;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection.Emit;
+using System.Text;
+using HarmonyLib;
+using Overload;
 
-namespace GameMod
-{
-
-    // Stock game shows 60/30hz for what is actually "full/half" sync rates in Unity, simply change labels
+namespace GameMod.Patches {
+    /// <summary>
+    /// Mod: VSync
+    /// Author: Tobias
+    /// 
+    /// Stock game shows 60/30hz for what is actually "full/half" sync rates in Unity, simply change labels
+    /// </summary>
     [HarmonyPatch(typeof(MenuManager), "GetVSyncSetting")]
-    class VSync_MenuManager_GetVSyncSetting
-    {
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
-        {
-            foreach (var code in codes)
-            {
+    class MenuManager_GetVSyncSetting {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes) {
+            foreach (var code in codes) {
                 if (code.opcode == OpCodes.Ldstr && (string)code.operand == "60 HZ")
                     code.operand = "100% MONITOR RATE";
 
@@ -25,21 +28,22 @@ namespace GameMod
         }
     }
 
-    // Stock game did not actually implement reverse arrow on vsync option
+    /// <summary>
+    /// Mod: VSync
+    /// Author: Tobias
+    /// 
+    /// Stock game did not actually implement reverse arrow on vsync option
+    /// </summary>
     [HarmonyPatch(typeof(MenuManager), "GraphicsOptionsUpdate")]
-    class VSync_MenuManager_GraphicsOptionsUpdate
-    {
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
-        {
+    class VSync_MenuManager_GraphicsOptionsUpdate {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes) {
             int state = 0;
-            foreach (var code in codes)
-            {
+            foreach (var code in codes) {
 
                 if (state == 0 && code.opcode == OpCodes.Ldsfld && code.operand == AccessTools.Field(typeof(MenuManager), "gfx_vsync"))
                     state = 1;
 
-                if (state == 1 && code.opcode == OpCodes.Ldc_I4_3)
-                {
+                if (state == 1 && code.opcode == OpCodes.Ldc_I4_3) {
                     // Original: MenuManager.gfx_vsync = (MenuManager.gfx_vsync + 3 - 1) % 3
                     // New:      MenuManager.gfx_vsync = (MenuManager.gfx_vsync + 3 + 1 - 1 + UIManager.m_select_dir) % 3
                     yield return new CodeInstruction(OpCodes.Ldc_I4_1);
